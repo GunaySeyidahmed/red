31cbe35829a7d57f1e445926effddc5d0cbaacd1
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2018-11-19 00:05:36 -0800

    commit2

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
     $('#search').click(function () {
-        var search=$('.search-result');
+        var search=$('.right-side .search-result');
         search.toggleClass('d-none');
         $("#search i").toggleClass('search-active');
+        $(".right-side .title").toggleClass('d-none');
+        $('.right-side .overlay').toggleClass('overlay-active');
     });
+
     $('.remove').click(function () {
         $(".search .search-input").val("");
     })
+
     $('#menuToggle input').click(function () {
         $('#menu').toggleClass('d-none');
     });
